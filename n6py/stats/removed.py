4945b45291ee2cdc7c5cfc1609228e7f94ef1f90
Sergej Samsonenko <contact@sergej.codes>
2022-11-26 20:54:45 +0100

    fix: return sequence and collection length

--- n6py/stats/removed.py
+++ n6py/stats/removed.py
@@ -5,17 +5,17 @@ from typing import Collection, Sequence, Union
 
 
 def removed(
-    new: Union[Union[int, float], Sequence, Collection],
-    old: Union[Union[int, float], Sequence, Collection],
+    new: Union[int, Sequence, Collection],
+    old: Union[int, Sequence, Collection],
 ):
     """
     Return a stats string about the difference between the old value and the new one.
 
     Parameters
     ----------
-    new : int, float, Sequence or Collection
+    new : int, Sequence or Collection
         Number of new values or new values.
-    old : int, float, Sequence or Collection
+    old : int, Sequence or Collection
         Number of new values or new values.
 
     Returns
@@ -28,21 +28,27 @@ def removed(
     >>> rm_stat = removed(50, 100)
     >>> print(rm_stat)
     Remaining: 50/100 | Removed: 50 - 50.00%
-    \u200B
+
     >>> rm_stat = removed([1, 2], [1, 2, 3, 4])
     >>> print(rm_stat)
     Remaining: 2/4 | Removed: 2 - 50.00%
     """
-    rm_num: Union[int, float] = 0
+    rm_old: int = 0
+    rm_new: int = 0
+    rm_num: int = 0
     rm_percentage: Union[int, float] = 0
 
-    if isinstance(new, (int, float)) and isinstance(old, (int, float)):
-        rm_num = old - new
+    if isinstance(new, int) and isinstance(old, int):
+        rm_old = old
+        rm_new = new
+        rm_num = rm_old - rm_new
         rm_percentage = 100 / old * rm_num
     elif isinstance(new, (Sequence, Collection)) and isinstance(
         old, (Sequence, Collection)
     ):
-        rm_num = len(old) - len(new)
+        rm_old = len(old)
+        rm_new = len(new)
+        rm_num = rm_old - rm_new
         rm_percentage = 100 / len(old) * rm_num
 
-    return f"Remaining: {new}/{old} | Removed: {rm_num} - {rm_percentage:.2f}%"
+    return f"Remaining: {rm_new}/{rm_old} | Removed: {rm_num} - {rm_percentage:.2f}%"
